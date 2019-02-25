0a7beeceebd5d40aa864b11746a5d07db19d6f63
jhonata <jhonatafiap@gmail.com>
2019-02-25 10:32:29 -0300

    'inicio'

---
+++ index.js
@@ -0,0 +1,13 @@
+const Methods = {
+    init() {
+        Methods.init();
+    },
+
+    menu(){
+        
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    Methods.init();
+});
