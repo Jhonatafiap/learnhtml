02484b2a5036954d6a423d0c422106f15b98cc14
jhonata <jhonatafiap@gmail.com>
2019-02-25 10:38:17 -0300

    'menu'

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const Methods = {
     init() {
-        Methods.init();
+        Methods.menu();
     },
 
     menu(){
-        
+        const btn = document.querySelector('.hamburguer');
+        const nav = document.querySelector('header nav');
+
+        btn.addEventListener('click', ev => {
+            const $this = ev.currentTarget;
+            
+            $this.classList.toggle('is--active');
+            nav.classList.toggle('is--active');
+        });
     }
 };
 
